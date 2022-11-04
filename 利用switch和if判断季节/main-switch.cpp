24af9c50caf0fa449e7066fa9d1135c6e02ec0e8
Akari <lh@lhcloud.com.cn>
2022-11-05 01:03:00 +0800

    Update main-switch.cpp
    Signed-off-by: Akari <lh@lhcloud.com.cn>

--- 利用switch和if判断季节/main-switch.cpp
+++ 利用switch和if判断季节/main-switch.cpp
@@ -8,17 +8,17 @@ int main()
     {
         case 12:
         case 1:
-        case 2:printf("%d月是冬季",a);break;
+        case 2:printf("%d月是冬季\n",a);break;
         case 3:
         case 4:
-        case 5:printf("%d月是春季",a);break;
+        case 5:printf("%d月是春季\n",a);break;
         case 6:
         case 7:
-        case 8:printf("%d月是夏季",a);break;
+        case 8:printf("%d月是夏季\n",a);break;
         case 9:
         case 10:
-        case 11:printf("%d月是球季",a);break;
-        default:printf("Error");
+        case 11:printf("%d月是球季\n",a);break;
+        default:printf("Error\n");
     }
     return 0;
 }
