2cd2f2528665aebea2265167de87712105e0da27
Akari <lh@lhcloud.com.cn>
2022-11-05 00:47:44 +0800

    Create main-switch.cpp
    Signed-off-by: Akari <lh@lhcloud.com.cn>

---
+++ 利用switch和if判断季节/main-switch.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("请输入月份：");
+    scanf("%d",&a);
+    switch (a) 
+    {
+        case 12:
+        case 1:
+        case 2:printf("%d月是冬季",a);break;
+        case 3:
+        case 4:
+        case 5:printf("%d月是春季",a);break;
+        case 6:
+        case 7:
+        case 8:printf("%d月是夏季",a);break;
+        case 9:
+        case 10:
+        case 11:printf("%d月是球季",a);break;
+        default:printf("Error");
+    }
+    return 0;
+}
